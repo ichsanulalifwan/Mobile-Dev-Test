2eb0e0029b7900a5b501be1db7f317c4f8052a75
ichsanulalifwan <ichsanulalifwan@gmail.com>
2022-09-13 06:21:18 +0800

    implement main activity

--- app/src/main/java/com/example/mobiledevtest/MainActivity.kt
+++ app/src/main/java/com/example/mobiledevtest/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.mobiledevtest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import com.example.mobiledevtest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupNavigation()
+    }
+
+    // Set Navigation Component
+    private fun setupNavigation() {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
     }
 }
